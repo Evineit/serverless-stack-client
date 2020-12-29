3ed4b9b5bc33e1c85808d9d3e2e1624ad551b6fe
Kevin R <seidaikevin@gmail.com>
2020-12-28 21:06:15 -0600

    Setting up our React App

---
+++ src/config.js
@@ -0,0 +1,17 @@
+const config = {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-api-bs3",
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://vncnpwiri3.execute-api.us-east-1.amazonaws.com/prod",
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_FsNa9122r",
+      APP_CLIENT_ID: "6qa728j404mkou3m4imq6jesu8",
+      IDENTITY_POOL_ID: "us-east-1:062dcf39-3b8e-4b9e-8727-ef2c2dd58b09",
+    },
+  };
+  export default config;
